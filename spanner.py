d142dbb8ec400c732e06304b1abfd8d31e5c1a3f
karankamboj <47560773+karankamboj@users.noreply.github.com>
2024-10-17 22:21:29 -0700

    Insert Data to Spanner

--- spanner.py
+++ spanner.py
@@ -6,13 +6,32 @@ def connectDatabase():
     database = instance.database('karan-db')
     return database
 
-def getData(database):
-    query = "SELECT id FROM events"
-    with database.snapshot() as snapshot:
-        results = snapshot.execute_sql(query)
-    for row in results:
-        print(row)
+def insertData(database):
+    try:
+        dataToInsert = [(2,), (3,), (4,), (5,)]
+        with database.batch() as batch:
+            batch.insert(
+                table='events',
+                columns=['id'], 
+                values=dataToInsert  
+            )
+        print("Data Inserted Successfuly")
+    except Exception as e:
+        print(f"Data insertion failed because {e}")
+        
+def fetchData(database):
+    try:
+        query = "SELECT id FROM events"
+        with database.snapshot() as snapshot:
+            results = snapshot.execute_sql(query)
+        for row in results:
+            print(row)
+
+        print("Data Fetched Successfuly")
+    except Exception as e:
+        print(f"Data Fetch failed because {e}")
     
 if __name__=="__main__":
     db = connectDatabase()
-    getData(db)
+    insertData(db)
+    fetchData(db)
