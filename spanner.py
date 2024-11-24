9003ea1082cb49bb9c71d4c1ecccd9b3bdd6423c
karankamboj <47560773+karankamboj@users.noreply.github.com>
2024-11-24 00:51:37 -0700

    add service account

--- spanner.py
+++ spanner.py
@@ -1,7 +1,15 @@
 from google.cloud import spanner
+from google.auth import default
+from google.oauth2 import service_account
 
 def connectDatabase():
-    spanner_client = spanner.Client()
+    json_key_path = "C:\\Users\\Acer\\Documents\\Study\\DDS\\BookMyEvent\\keys\\spanner-project-439003-fda10b1b5add.json"  # Replace with your key file path
+    credentials = service_account.Credentials.from_service_account_file(json_key_path)
+
+    spanner_client = spanner.Client(credentials=credentials, project="spanner-project-439003")
+    
+    print(f"Connected to project: {spanner_client.project}")
+
     instance = spanner_client.instance('spanner-dds')
     database = instance.database('karan-db')
     return database
