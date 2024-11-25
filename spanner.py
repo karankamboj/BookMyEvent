b915ae528fc704f284fff1efeafe97fb4cc6d781
Shandeep kamboj <shan.deep.kamboj@gmail.com>
2024-11-25 16:53:25 -0700

    Add operation type support

--- spanner.py
+++ spanner.py
@@ -3,7 +3,13 @@ from google.cloud import spanner
 from google.auth import default
 from google.oauth2 import service_account
 from datetime import datetime, timezone
+from enum import Enum
 
+
+class OperationType(Enum):
+    READ = "READ"
+    WRITE = "WRITE"
+    
 def connect_to_spanner_instance(service_account_path, instance_id, database_id):
     # Set the environment variable for authentication
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_path
@@ -12,7 +18,7 @@ def connect_to_spanner_instance(service_account_path, instance_id, database_id):
     database = instance.database(database_id)
     return database
 
-def getkambojDbInstance():
+def getReadDbInstance():
     service_account_path_1_kkamobj = "./keys/spanner-project-439003-fda10b1b5add.json"
     instance_id_1_kkamboj = "spanner-dds"
     database_id_1_kkamboj = "karan-db"
@@ -20,18 +26,19 @@ def getkambojDbInstance():
     source_db = connect_to_spanner_instance(service_account_path_1_kkamobj, instance_id_1_kkamboj, database_id_1_kkamboj)
     return source_db
 
-def getnchaudDbInstance():
+def getWriteDbInstance():
     service_account_path_2_nchaud = "./keys/key_2_nchaud.json"
     instance_id_2_nchaud = "bookmyevent"
     database_id_2_nchaud = "ddsdb"
     source_db = connect_to_spanner_instance(service_account_path_2_nchaud, instance_id_2_nchaud, database_id_2_nchaud)
     return source_db
 
-def connectDatabase():
-    database = getkambojDbInstance()
+def connectDatabase(OperationType=None):
+
+    database = getReadDbInstance()
     return database
 
-def insertData(database, tableName, columns, dataToInsert):
+def insertDataUtil(database, tableName, columns, dataToInsert):
     try:
         with database.batch() as batch:
             batch.insert(
@@ -42,8 +49,14 @@ def insertData(database, tableName, columns, dataToInsert):
         return "Data Inserted Successfuly"
     except Exception as e:
         return f"Data insertion failed because {e}"
+
+def insertData(database, tableName, columns, dataToInsert):
+    return insertDataUtil(database, tableName, columns, dataToInsert)
         
 def fetchData(database, tableName):
+    return fetchDataUtil(database, tableName)
+
+def fetchDataUtil(database, tableName):
     try:
         query = "SELECT * FROM "+tableName
         with database.snapshot() as snapshot:
@@ -54,8 +67,8 @@ def fetchData(database, tableName):
     
 if __name__=="__main__":
     print("HERE")
-    source_db_1 = getkambojDbInstance()
-    source_db_2 = getnchaudDbInstance()
+    source_db_1 = getReadDbInstance()
+    source_db_2 = getWriteDbInstance()
 
     print(source_db_1)
     print(source_db_2)
