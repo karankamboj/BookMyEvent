9d02b84acd07795652c6bdd9d57cbd9a87d40e5c
Nishtha Chaudhary <nishtha@Nishthas-MacBook-Pro.local>
2024-11-26 20:36:08 -0700

    Update and insert sync!

--- syncDatabase.py
+++ syncDatabase.py
@@ -18,23 +18,22 @@ def sync_spanner_databases(source_db, target_db):
         source_db (spanner.Database): The source Spanner database instance.
         target_db (spanner.Database): The target Spanner database instance.
     """
-    def sync_table_data(table_name):
-       with source_db.snapshot() as snapshot:
-        column_query = f"""
-        SELECT column_name
-        FROM information_schema.columns
-        WHERE table_name = '{table_name}'
-        """
-        columns = [row[0] for row in snapshot.execute_sql(column_query)]
-        print(f"Columns for {table_name}: {columns}")
-
+    def sync_table_data(table_name):  
+        # Create mutations
+        with source_db.snapshot() as snapshot:
+            column_query = f"""
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name = '{table_name}'
+            """
+            columns = [row[0] for row in snapshot.execute_sql(column_query)]
+            print(f"Columns for {table_name}: {columns}")
 
         # Fetch data from the source table
         with source_db.snapshot() as snapshot:
             data_query = f"SELECT * FROM {table_name}"
             rows = list(snapshot.execute_sql(data_query))
             print(f"Fetched {len(rows)} rows from {table_name}")
-
         
         if rows:
             for row in rows:
@@ -42,7 +41,7 @@ def sync_spanner_databases(source_db, target_db):
                 data_to_insert = [tuple(row_dict[col] for col in columns)]  # Create a tuple for each row
                 try:
                     with target_db.batch() as batch:
-                        batch.insert(
+                        batch.insert_or_update(
                             table=table_name,
                             columns=columns,
                             values=data_to_insert,
@@ -73,7 +72,7 @@ def startSyncJob():
     Schedules the sync task to run every 5 minutes.
     """
     # Schedule the sync to run every 1 minutes
-    schedule.every(60).seconds.do(syncData)
+    schedule.every(1).seconds.do(syncData)
     
     print("Sync job started.")
     
